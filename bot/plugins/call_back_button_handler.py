e52461cb46f790c4c8d0ed4d955c2c9cefaca098
Mugeñ ®ao <84485574+theRawrrr@users.noreply.github.com>
2021-06-17 20:25:36 +0530

    Update call_back_button_handler.py

--- bot/plugins/call_back_button_handler.py
+++ bot/plugins/call_back_button_handler.py
@@ -37,7 +37,7 @@ async def button(bot, update: CallbackQuery):
     if (update.from_user.id == update.message.reply_to_message.from_user.id) or g:
         print(cb_data)
         if cb_data == "fuckingdo":
-            if update.from_user.id in AUTH_USERS:
+            if update.from_user.id:
                 status = DOWNLOAD_LOCATION + "/status.json"
                 with open(status, 'r+') as f:
                     statusMsg = json.load(f)
